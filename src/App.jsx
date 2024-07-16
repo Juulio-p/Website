49e4a46aed720a37a5aff218240cd5a5d35d53a4
Julio C. Prudencio <pjulio1234@gmail.com>
2024-07-15 22:05:12 -0700

    Refactored some code and styling changes to button

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,42 @@ import { useState } from 'react'
 import './App.css'
 
 
+
+function LoginForm() {
+  return(
+    <form>
+      <div>
+        <label htmlFor="username">  UserName:</label>
+        <input type= "username" id="username" />
+         
+        </div>
+
+        <div> 
+        <label htmlFor="password">  Password:</label>
+        <input type= "password" id="password" /> 
+        </div>
+
+        <div>
+          
+        </div>
+
+        <button  name="login"  type="submit" id="login">Log In</button>
+
+    </form>
+
+    
+  ); 
+}
+
 function App() {
 
   return (
     <>
 
-    <div id= "div1">
-    <label for="text input"> Text input:</label>
-    <input
-    type="text"
-    id="text"
-    />
+    <div id="div1">    
+      <LoginForm /> 
     </div>
-    
-    <div id ="div2">
-    <label for= "file"> File Input:</label>
-    <input 
-    id="file" 
-    type="file"
-    />
-    </div>
-
-
-    <div id="div3"> 
-      <input id="sub" type = "submit"></input>
-    </div>
-
     </>
   )
 }
-
 export default App
