f706a892525f2949b7752c69e763381d647d8470
Julio C. Prudencio <pjulio1234@gmail.com>
2024-07-23 15:27:13 -0700

    changed svg picture and seperated the login page to a diff file, and started a welcome page, going to try routing now

---
+++ src/welcome_page.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function Welcome_page () { 
+
+return (
+
+<div> Your now one of our elite employees</div>
+);
+}
+
+export default Welcome_page;
