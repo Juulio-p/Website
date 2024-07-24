03682a6ec9e932c66b1c6782d42fbe650c33466e
Julio C. Prudencio <pjulio1234@gmail.com>
2024-07-23 17:50:01 -0700

    Started basics of routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import './App.css'
 import LoginForm from './Login_page'
 import Welcome_page from './welcome_page'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 
 function App() {
   return (
     <>
-    <div id="div1">    
-      <LoginForm /> 
-    </div>
+    <Router> 
+      <Routes> 
+        <Route path="/" element={<LoginForm/>}/> 
+      
+  
+  
+      <Route path='/Welcomepg' element={<Welcome_page />}/> 
+      
 
-    <div>
-   <Welcome_page /> 
-    </div>
+
+    </Routes>
+    </Router>
     </>
   )
 }
